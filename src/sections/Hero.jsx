a603530f4059016dbf7af160ef8c12327aaf1fc3
nooorulain <noorulannfatima@gmail.com>
2026-01-23 17:47:40 +0500

    final commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,4 @@
 import { Canvas } from "@react-three/fiber";
-import { Planet } from "../components/Planet";
 import { Environment, Float, Lightformer } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
 import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
